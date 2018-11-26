13f3dbc506221509af664a9251caa087b4e5b1b8
Ajit Kumar Goel <ajitgoel@gmail.com>
2018-11-25 22:38:37 -0600

    changes

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React from 'react';
+//import { Link } from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import FlatButton from 'material-ui/FlatButton';
 
-const NavBar=() => <AppBar title="Pixabay Image Finder"/>
+const NavigationLinks = () => (
+  <ToolbarGroup>
+    <FlatButton label="Login"/>
+    <FlatButton label="SignUp"/>
+  </ToolbarGroup> 
+);
+
+const NavBar=() => <AppBar title="Pixabay Image Finder" iconElementRight={<NavigationLinks/>}/>
 
 export default NavBar;
